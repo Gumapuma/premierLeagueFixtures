4994d4e4c25c02f3665c94d0379bfcfb186ab462
MurlokReal <kirill.skrypnik080@gmail.com>
2024-08-12 13:36:31 +0300

    Добавлен поиск по матчам

--- app/src/main/java/com/murlok/premierleaguefixtures/view/stateHolders/MainViewModel.kt
+++ app/src/main/java/com/murlok/premierleaguefixtures/view/stateHolders/MainViewModel.kt
@@ -1,21 +1,34 @@
 package com.murlok.premierleaguefixtures.view.stateHolders
 
+import android.util.Log
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.murlok.premierleaguefixtures.data.model.FootballMatch
-import com.murlok.premierleaguefixtures.data.network.APIForRetrofit
 import com.murlok.premierleaguefixtures.data.repository.MainRepository
-import com.murlok.premierleaguefixtures.data.repository.MainRepositoryImpl
-import com.murlok.premierleaguefixtures.data.room.MatchDatabase
 import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.combine
+import kotlinx.coroutines.flow.stateIn
 import kotlinx.coroutines.launch
-import retrofit2.Retrofit
-import retrofit2.converter.gson.GsonConverterFactory
+
 
 class MainViewModel(private val repository: MainRepository) : ViewModel() {
     private val _matches = MutableStateFlow<List<FootballMatch>>(emptyList())
     val matches: StateFlow<List<FootballMatch>> get() = _matches
+
+    private val _searchQuery = MutableStateFlow("")
+    val searchQuery: StateFlow<String> get() = _searchQuery
+
+    val filteredMatches: StateFlow<List<FootballMatch>> = _searchQuery
+        .combine(_matches) { query, matches ->
+            Log.d("MainViewModel", "Filtering matches with query: $query")
+            if (query.isEmpty()) {
+                matches
+            } else {
+                matches.filter { it.homeTeam.contains(query, true) || it.awayTeam.contains(query, true) }
+            }
+        }.stateIn(viewModelScope, SharingStarted.WhileSubscribed(), emptyList())
 
     init {
         viewModelScope.launch {
@@ -34,5 +47,10 @@ class MainViewModel(private val repository: MainRepository) : ViewModel() {
             repository.updateLocalMatches()
             fetchLocalMatches()
         }
+    }
+
+    fun onSearchQueryChanged(query: String) {
+        _searchQuery.value = query
+        Log.d("MainViewModel", "Search query updated: $query")
     }
 }
